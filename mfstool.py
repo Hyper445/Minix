91ee4cb3f71def496b3acb3c889693f58e8c9978
Hyper445 <Dennisvet4@hotmail.com>
2022-05-17 06:16:25 -0400

    Initial commit

---
+++ mfstool.py
@@ -0,0 +1,51 @@
+import sys
+import struct
+
+BLOCK_SIZE = 1024
+
+
+def parse_superblock(sbdata):
+    sbdict = {}
+
+    idx = 0
+    (sbdict["ninodes"],) = struct.unpack("<H", sbdata[idx : idx + 2])
+    idx += 2
+    (sbdict["nzones"],) = struct.unpack("<H", sbdata[idx : idx + 2])
+    idx += 2
+    (sbdict["imap_blocks"],) = struct.unpack("<H", sbdata[idx : idx + 2])
+    idx += 2
+    (sbdict["zmap_blocks"],) = struct.unpack("<H", sbdata[idx : idx + 2])
+    idx += 2
+    (sbdict["firstdatazones"],) = struct.unpack("<H", sbdata[idx : idx + 2])
+    idx += 2
+    (sbdict["log_zone_size"],) = struct.unpack("<H", sbdata[idx : idx + 2])
+    idx += 2
+    (sbdict["maxfilesize"],) = struct.unpack("<L", sbdata[idx : idx + 4])
+    idx += 4
+    (sbdict["magic"],) = struct.unpack("<H", sbdata[idx : idx + 2])
+    idx += 2
+    (sbdict["state"],) = struct.unpack("<H", sbdata[idx : idx + 2])
+    idx += 2
+
+
+    return sbdict
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: mfstool.py image command params")
+        sys.exit(0)
+
+    diskimg = sys.argv[1]
+    cmd = sys.argv[2]
+
+    with open(diskimg, "rb") as f:
+
+        # Skip boot block
+        f.seek(BLOCK_SIZE, 0)
+        # Read super block
+        sbdata = f.read(BLOCK_SIZE)
+
+        sbdict = parse_superblock(sbdata)
+
+        print(sbdict)
