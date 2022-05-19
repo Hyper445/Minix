982deced3bc56ae4e7b29d4ce6bd63e40ca75132
Hyper445 <Dennisvet4@hotmail.com>
2022-05-19 08:24:07 -0400

    Alle inodes uitgelezen denk ik

--- mfstool.py
+++ mfstool.py
@@ -35,10 +35,23 @@ def parse_superblock(sbdata):
 
 def parse_inodetable(sbdata):
 
-    (x,) = struct.unpack("<H", sbdata[0 : 2])
-    print(x)
+    sbinodeentry = {}
 
-    return x
+    idx = 0
+    (sbinodeentry["mode"],) = struct.unpack("<H", sbdata[idx : idx + 2])
+    idx += 2
+    (sbinodeentry["uid"],) = struct.unpack("<H", sbdata[idx : idx + 2])
+    idx += 2
+    (sbinodeentry["size"],) = struct.unpack("<L", sbdata[idx : idx + 4])
+    idx += 4
+    (sbinodeentry["time"],) = struct.unpack("<L", sbdata[idx : idx + 4])
+    idx += 4
+    (sbinodeentry["gid"],) = struct.unpack("<B", sbdata[idx : idx + 1])
+    idx += 1
+    (sbinodeentry["links"],) = struct.unpack("<B", sbdata[idx : idx + 1])
+    idx += 1
+
+    return sbinodeentry
 
 
 if __name__ == "__main__":
@@ -56,31 +69,32 @@ if __name__ == "__main__":
         # Read super block
         sbdata = f.read(BLOCK_SIZE)
 
-        # Save data in superblock in sbdict
+        # Save data from superblock in sbdict
         sbdict = parse_superblock(sbdata)
 
-        #f.seek(BLOCK_SIZE, 1)
+        # Move past superblock
+        f.seek(BLOCK_SIZE, 1)
 
         # Get sizes of inodemap and zonemap
         INODEMAP_BLOCK_SIZE = sbdict['imap_blocks'] * BLOCK_SIZE
         ZONEMAP_BLOCK_SIZE = sbdict['zmap_blocks'] * BLOCK_SIZE
 
-        # Skip these two blocks
+        # Skip inodemap block and zonemap block
         f.seek(INODEMAP_BLOCK_SIZE + ZONEMAP_BLOCK_SIZE, 1)
 
-
         # Calculate the amount of blocks needed for all the inodes
-        INODETABLE_SIZE = math.ceil(sbdict['ninodes'] * 32 / BLOCK_SIZE)
+        INODETABLE_BLOCKS = math.ceil(sbdict['ninodes'] * 32 / BLOCK_SIZE)
 
-        sbdata = f.read(INODETABLE_SIZE)
+        sbdata = f.read(32)
 
         sbinodetable = {}
 
-        for i in range(INODETABLE_SIZE):
+        for i in range(sbdict['ninodes']):
             sbinodetable[i] = parse_inodetable(sbdata)
-            f.seek(BLOCK_SIZE, 1)
-            sbdata = f.read(BLOCK_SIZE)
+            f.seek(32, 1)
+            sbdata = f.read(32)
 
-        
-        print(sbinodetable)
+        for i in range(len(sbinodetable)):
+            print(sbinodetable[i])
+        #print(sbinodetable)
         print(sbdict)
